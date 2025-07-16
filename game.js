3a8ec0c6559bbda6a2db6e122a2a710aea16b679
G0lf-Thanap0n <thanaponphunthawi@gmail.com>
2025-07-16 20:34:29 +0700

    game.js solution

--- game.js
+++ game.js
@@ -9,17 +9,23 @@ let x = 0;
 let y = 0; // Start at A (0,0) -> [row][col]
 
 const moves = [];
-moves.push(board[x][y]); // A
+moves.push(board[x][y]); // A 
 
 // Move Right to B
 y++;
-moves.push(board[x][y]); // B
+moves.push(board[x][y]); // B (0,1)
 
 // Move Right to C
+y++;
+moves.push(board[x][y]); // C (0,2)
 
 // Move Down to F
+x++;
+moves.push(board[x][y]); // F (1,2)
 
 // Move left to E
+y--;
+moves.push(board[x][y]); // E (1,1)
 
 console.log("Path:", moves.join(" → "));
 // Output: Path: A → B → C → F → E
